13c423b2557ecb7c232cd215a2138f4f83b3379a
Gabriel L. Maljkovich <gabriel.l.maljkovich@gmail.com>
2024-03-27 15:19:04 -0300

    persisted vector store

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from llama_index.core import VectorStoreIndex, ServiceContext, Document
+from llama_index.core import VectorStoreIndex, StorageContext, Settings, load_index_from_storage
 from llama_index.llms.openai import OpenAI
 import openai
 from llama_index.readers.file import PDFReader
@@ -7,36 +7,39 @@ from pathlib import Path
 
 openai.api_key = st.secrets.openai_key
 
+Settings.llm = OpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.5,
+    system_prompt=(
+        "You are a Gabriel Leonardo Maljkovich, a Software Engineer, and your job is to answer technical questions about your resume / Curriculum Vitae."
+        " Assume that all questions are related to your work experience. "
+        "Keep your answers technical and based on facts – do not hallucinate jobs, positions or technologies. "
+    )
+)
+
 # Page title
-st.set_page_config(page_title='ML Model Building', page_icon='🤖')
-st.title('🤖 RAG CV Chat')
+st.set_page_config(page_title='AI Resume', page_icon='🤖')
+st.title('🤖 AI Resumé')
 
 if "messages" not in st.session_state.keys(): # Initialize the chat message history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+        {"role": "assistant", "content": "Hi, I'm Gabriel. Ask me a question about my work experience!"}
     ]
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+    with st.spinner(text="Loading and indexing work history – hang tight! This should take 1-2 minutes."):
         reader = PDFReader()
         docs = reader.load_data(file=Path("./data/cv_gmaljkovich_english.pdf"))
-        service_context = ServiceContext.from_defaults(
-            llm=OpenAI(
-                model="gpt-3.5-turbo",
-                temperature=0.5,
-                system_prompt=(
-                    "You are a Gabriel Leonardo Maljkovich, a Software Engineer, and your job is to answer technical questions about your resume / Curriculum Vitae."
-                    " Assume that all questions are related to your work experience. "
-                    "Keep your answers technical and based on facts – do not hallucinate jobs, positions or technologies. "
-                    )
-                )
-            )
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        index = VectorStoreIndex.from_documents(docs)
+        index.set_index_id("glm_cv")
+        index.storage_context.persist("./data/vector_store")
         return index
 
 # llama index
-index = load_data()
+# index = load_data()
+storage_context = StorageContext.from_defaults(persist_dir="data/vector_store")
+index = load_index_from_storage(storage_context, index_id="glm_cv")
 
 chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
