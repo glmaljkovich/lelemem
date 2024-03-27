17fee36bb7df509872bcbd7893cb3d153007788e
Gabriel L. Maljkovich <gabriel.l.maljkovich@gmail.com>
2024-03-27 15:38:15 +0000

    correct imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext, Document
-from llama_index.llms import OpenAI
+from llama_index.core import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms.openai import OpenAI
 import openai
-from llama_index import SimpleDirectoryReader
+from llama_index.core import SimpleDirectoryReader
 
 openai.api_key = st.secrets.openai_key
 
